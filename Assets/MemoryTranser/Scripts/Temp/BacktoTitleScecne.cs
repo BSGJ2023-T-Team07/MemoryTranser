a88da9674444da3033108eca555b4be7f1ab8a2d
inutamago_dogegg <inutamago.dogegg@gmail.com>
2023-06-24 10:04:32 +0900

    debug

--- Assets/MemoryTranser/Scripts/Temp/BacktoTitleScecne.cs
+++ Assets/MemoryTranser/Scripts/Temp/BacktoTitleScecne.cs
@@ -1,10 +1,15 @@
+using MemoryTranser.Scripts.Game.Sound;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace MemoryTranser.Scripts.Temp {
     public class BackToTitleScene : MonoBehaviour {
         private void Update() {
-            if (Input.GetKeyDown(KeyCode.Escape)) SceneManager.LoadScene("MemoryTranser/Scenes/TitleScene");
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                BgmManager.I.SetBgmVolume(0f);
+                SeManager.I.SetSeVolume(0f);
+                SceneManager.LoadScene("MemoryTranser/Scenes/TitleScene");
+            }
         }
     }
 }

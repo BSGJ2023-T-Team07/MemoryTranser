1edded4d0d38c83d3132c2eb198bcae4b5205e64
inutamago_dogegg <inutamago.dogegg@gmail.com>
2023-06-30 23:41:15 +0900

    feat:重み付きランダムのプログラムを書いた

---
+++ Assets/MemoryTranser/Scripts/Game/Util/AliasMethod.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MemoryTranser.Scripts.Game.Util {
+    public class AliasMethod {
+        private int _n;
+        private float[] _probabilities;
+        private int[] _alias;
+
+        public void Constructor(float[] weights) {
+            _n = weights.Length;
+            var sum = weights.Sum(x => x);
+            var p = weights.Select(x => x / sum).ToArray();
+
+            _probabilities = new float[_n];
+            _alias = new int[_n];
+
+            var small = new Queue<int>();
+            var large = new Queue<int>();
+
+            foreach (var (pp, i) in p.Select((pp, i) => (pp, i))) {
+                if (pp < 1) {
+                    small.Enqueue(i);
+                }
+                else {
+                    large.Enqueue(i);
+                }
+            }
+
+            while (small.Count > 0 && large.Count > 0) {
+                var l = small.Dequeue();
+                var g = large.Dequeue();
+
+                _probabilities[l] = p[l];
+                _alias[l] = g;
+
+                p[g] = p[g] + p[l] - 1;
+
+                if (p[g] < 1) {
+                    small.Enqueue(g);
+                }
+                else {
+                    large.Enqueue(g);
+                }
+            }
+
+            while (large.Count > 0) {
+                var g = large.Dequeue();
+                _probabilities[g] = 1;
+            }
+
+            while (small.Count > 0) {
+                var l = small.Dequeue();
+                _probabilities[l] = 1;
+            }
+        }
+
+        public int Roll() {
+            var i = new System.Random().Next(_n);
+            return new System.Random().NextDouble() < _probabilities[i] ? i : _alias[i];
+        }
+    }
+}

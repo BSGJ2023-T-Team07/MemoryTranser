ab9940d06e6fccfb364f3241c08cd217c8532afe
inutamago_dogegg <inutamago.dogegg@gmail.com>
2023-07-03 23:04:58 +0900

    feat:定期的に集中力の減少速度が上がるようになった

--- Assets/MemoryTranser/Scripts/Game/Concentration/ConcentrationManager.cs
+++ Assets/MemoryTranser/Scripts/Game/Concentration/ConcentrationManager.cs
@@ -13,9 +13,18 @@ namespace MemoryTranser.Scripts.Game.Concentration {
 
         #region 変数の定義
 
-        [SerializeField] private float maxConcentration = 60f;
+        [Header("完全放置で集中力が持つ時間(秒)")] [SerializeField]
+        private float maxConcentration;
+
+        [Header("減少速度が上がる間隔(秒)")] [SerializeField]
+        private float additionalDecreaseInterval;
+
+        [Header("↑の時間が経った時に加算される減少速度の割合(％)")] [SerializeField]
+        private float additionalDecreasePercent;
 
         private float _remainingConcentration;
+        private float _remainingTimeForAdditionalDecrease;
+        private float _decreaseMultiplier = 1f;
 
         private bool _decreaseFlag;
 
@@ -28,7 +37,13 @@ namespace MemoryTranser.Scripts.Game.Concentration {
             concentrationShower.SetValue(_remainingConcentration / maxConcentration);
 
             if (_decreaseFlag) {
-                _remainingConcentration -= Time.deltaTime;
+                _remainingConcentration -= Time.deltaTime * _decreaseMultiplier;
+                _remainingTimeForAdditionalDecrease -= Time.deltaTime;
+            }
+
+            if (_remainingTimeForAdditionalDecrease < 0) {
+                _remainingTimeForAdditionalDecrease = additionalDecreaseInterval;
+                _decreaseMultiplier += additionalDecreasePercent / 100;
             }
 
             if (_remainingConcentration < 0) {
@@ -43,6 +58,7 @@ namespace MemoryTranser.Scripts.Game.Concentration {
 
         public void OnStateChangedToInitializing() {
             _remainingConcentration = maxConcentration;
+            _remainingTimeForAdditionalDecrease = additionalDecreaseInterval;
         }
 
         public void OnStateChangedToReady() {

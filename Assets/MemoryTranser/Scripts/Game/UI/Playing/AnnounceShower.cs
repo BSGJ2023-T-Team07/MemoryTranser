51f2f0bb7aba1119e6f12df34e1cce987aa459d5
inutamago_dogegg <inutamago.dogegg@gmail.com>
2023-07-12 00:36:52 +0900

    feat:アナウンスを実装した

--- Assets/MemoryTranser/Scripts/Game/UI/Playing/AnnounceShower.cs
+++ Assets/MemoryTranser/Scripts/Game/UI/Playing/AnnounceShower.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using MemoryTranser.Scripts.Game.BrainEvent;
 using MemoryTranser.Scripts.Game.Util;
@@ -8,32 +11,62 @@ namespace MemoryTranser.Scripts.Game.UI.Playing {
     public class AnnounceShower : SingletonMonoBehaviour<AnnounceShower> {
         protected override bool DontDestroy => false;
 
-        [SerializeField] private Transform announceTextParent;
+        [SerializeField] private Transform announceTextBackGround;
         [SerializeField] private TextMeshProUGUI announcePrefab;
 
-        [Header("アナウンスの流れる速さ")] [SerializeField]
+        [Header("アナウンスの流れる速さ(ピクセル/秒)")] [SerializeField]
         private float announceMoveSpeed;
 
-        private Vector3 _defaultAnnounceTextPosition;
+        [Header("アナウンスの文章間の間隔(ピクセル)")] [SerializeField]
+        private float announceTextUnitIntervalDistance;
 
+        //アナウンスの文章1つが流れきる時間
+        private float _announceUnitDuration;
+
+        private Vector3 _defaultAnnounceTextLocalPosition;
         private Sequence _announceSequence;
+        private Queue<TextMeshProUGUI> _announceQueue = new();
 
         protected override void Awake() {
             base.Awake();
+
+            _announceSequence = DOTween.Sequence();
+
+            _defaultAnnounceTextLocalPosition = Vector3.right * (Constant.SCREEN_WIDTH / 2f);
+            _announceUnitDuration = Constant.SCREEN_WIDTH / announceMoveSpeed;
+
+            //改行をしないようにする
             announcePrefab.enableWordWrapping = false;
         }
 
-        private void AddAnnounceText(string text, float durationSec) {
-            var additionAnnounceText = Instantiate(announcePrefab, announceTextParent);
-            additionAnnounceText.SetText(text);
+        public async void AddAnnounceText(string text, float durationSec) {
+            var additionalTextUnit = GenerateNewTextUnit(text);
+
+            var unitIntervalSec =
+                (additionalTextUnit.preferredWidth + announceTextUnitIntervalDistance) / announceMoveSpeed;
+
+            var textUnitCount = (int)(durationSec / unitIntervalSec);
+
+            for (var i = 0; i < Mathf.Max(textUnitCount - 3, 1); i++) {
+                GenerateNewTextUnit(text);
+
+                await UniTask.Delay(
+                    TimeSpan.FromSeconds(unitIntervalSec));
+            }
         }
 
-        public void SetBrainEventTypeText(BrainEventType brainEventType, float durationSec) {
-            if (brainEventType == BrainEventType.None) {
-                return;
-            }
+        private TextMeshProUGUI GenerateNewTextUnit(string text) {
+            var newTextUnit = Instantiate(announcePrefab, announceTextBackGround);
+            newTextUnit.transform.localPosition = _defaultAnnounceTextLocalPosition;
+            newTextUnit.SetText(text);
 
-            AddAnnounceText($"{brainEventType.ToJapanese()}！！", durationSec);
+            //動かして消えるところまで定義しておく
+            newTextUnit.transform.DOLocalMoveX(-Constant.SCREEN_WIDTH - newTextUnit.preferredWidth,
+                _announceUnitDuration).SetRelative().SetEase(Ease.Linear).OnComplete(() => {
+                Destroy(_announceQueue.Peek().gameObject);
+            });
+
+            return newTextUnit;
         }
     }
 }
---
+++ Assets/MemoryTranser/Scripts/Game/UI/Playing/BrainEventTypeShower.cs
@@ -0,0 +1,70 @@
+using System;
+using MemoryTranser.Scripts.Game.BrainEvent;
+using MemoryTranser.Scripts.Game.Util;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace MemoryTranser.Scripts.Game.UI.Playing {
+    public class BrainEventTypeShower : MonoBehaviour {
+        [SerializeField] private PostProcessVolume postProcessVolume;
+
+        public void SetBrainEventShow(BrainEventType brainEventType, float durationSec) {
+            SetPostProcess(brainEventType);
+
+            if (brainEventType != BrainEventType.None) {
+                SetBrainEventAnnounce(brainEventType, durationSec);
+            }
+        }
+
+        private static void SetBrainEventAnnounce(BrainEventType brainEventType, float durationSec) {
+            AnnounceShower.I.AddAnnounceText(GetBrainEventText(brainEventType), durationSec);
+        }
+
+        private static string GetBrainEventText(BrainEventType brainEventType) {
+            return brainEventType switch {
+                BrainEventType.Blind => $"{brainEventType.ToJapanese()}発生中！",
+                BrainEventType.DesireOutbreak => $"{brainEventType.ToJapanese()}発生中！",
+                BrainEventType.InvertControl => $"{brainEventType.ToJapanese()}発生中！",
+                BrainEventType.AchievementOfStudy => $"{brainEventType.ToJapanese()}が出た！",
+                _ => throw new ArgumentOutOfRangeException(nameof(brainEventType), brainEventType, null)
+            };
+        }
+
+        private void SetPostProcess(BrainEventType brainEventType) {
+            switch (brainEventType) {
+                case BrainEventType.None:
+                    SetPostProcessForNone();
+                    break;
+                case BrainEventType.Blind:
+                    SetPostProcessForBlind();
+                    break;
+                case BrainEventType.DesireOutbreak:
+                    SetPostProcessForDesireOutBreak();
+                    break;
+                case BrainEventType.InvertControl:
+                    SetPostProcessForInvertControl();
+                    break;
+                case BrainEventType.AchievementOfStudy:
+                    SetPostProcessForAchievementOfStudy();
+                    break;
+                case BrainEventType.FeverTime:
+                    SetPostProcessForFeverTime();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(brainEventType), brainEventType, null);
+            }
+        }
+
+        private void SetPostProcessForNone() { }
+        private void SetPostProcessForDesireOutBreak() { }
+        private void SetPostProcessForBlind() { }
+        private void SetPostProcessForAchievementOfStudy() { }
+        private void SetPostProcessForInvertControl() { }
+
+        private void SetPostProcessForFeverTime() {
+            var bloom = postProcessVolume.profile.GetSetting<Bloom>();
+            bloom.active = true;
+            bloom.intensity.value = 1f;
+        }
+    }
+}

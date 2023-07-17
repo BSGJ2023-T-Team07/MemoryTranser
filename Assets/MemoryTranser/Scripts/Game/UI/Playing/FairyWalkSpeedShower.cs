ac2483a6f74dd528352a0076538ab501b63412c4
inutamago_dogegg <inutamago.dogegg@gmail.com>
2023-07-17 18:56:43 +0900

    improve

--- Assets/MemoryTranser/Scripts/Game/UI/Playing/FairyWalkSpeedShower.cs
+++ Assets/MemoryTranser/Scripts/Game/UI/Playing/FairyWalkSpeedShower.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 namespace MemoryTranser.Scripts.Game.UI.Playing {
     public class FairyWalkSpeedShower : MonoBehaviour {
         [SerializeField] private Slider fairyWalkSpeedSlider;
+        [SerializeField] private Image fairyWalkSpeedPict;
 
         [Header("値の更新アニメーションに何秒かかるか")] [SerializeField]
         private float updateAnimationSec;
+
+        [Header("ピクトさんのGradient")] [SerializeField]
+        private Gradient fairyWalkSpeedGradient;
 
         [Header("×2のときの値")] [SerializeField] private float twoValue;
         [Header("×1のときの値")] [SerializeField] private float oneValue;
@@ -19,6 +23,10 @@ namespace MemoryTranser.Scripts.Game.UI.Playing {
         }
 
         public void SetWalkSpeedSlider(float currentPlainWalkSpeed, float initialWalkSpeed) {
+            fairyWalkSpeedPict
+                .DOGradientColor(fairyWalkSpeedGradient, updateAnimationSec * 3f)
+                .SetLink(gameObject);
+
             if (currentPlainWalkSpeed < initialWalkSpeed) {
                 fairyWalkSpeedSlider.DOValue(
                     (oneValue - zeroValue) / initialWalkSpeed * currentPlainWalkSpeed + zeroValue,
